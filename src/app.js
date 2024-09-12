90050b24857a6f06449de507b92b4d1642038f1a
Theluther101 <afolabi12356@gmail.com>
2024-09-12 00:24:26 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 };
+function generateExcuse() {
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My girl",
+    "My shoe",
+    "My car"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "sleep", "food", "timer"];
+  let what = [
+    "my homework",
+    "my phone",
+    "the car",
+    "my cloth",
+    "my benz",
+    "the pastor"
+  ];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+    "when i woke up",
+    "when i eat",
+    "when i dance"
+  ];
+
+  function getRandomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  return `${getRandomItem(who)} ${getRandomItem(action)} ${getRandomItem(
+    what
+  )} ${getRandomItem(when)}.`;
+}
